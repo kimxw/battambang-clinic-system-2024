d5730c4d034795f1d9f5a49ddbfff14363af4664
Kimaya Wanjari <kimaya.wanjari@nuscomputing.com>
2024-06-12 15:51:49 +0900

    Added height and weight station funcationality and automatic bmi and category calculation as well as fetching from database to display existing data if any. Can add new data and also update existing data. BMI data's existance is confirmed by a display label. All data is on the heightAndWeight Table in the clinicdb.db

--- src/main/java/com/orb/battambang/checkupstation/CheckupMenuController.java
+++ src/main/java/com/orb/battambang/checkupstation/CheckupMenuController.java
@@ -1,32 +1,209 @@
 package com.orb.battambang.checkupstation;
 
 import com.orb.battambang.MainApp;
+import com.orb.battambang.util.Labels;
+
+import com.orb.battambang.connection.DatabaseConnection;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 
-public class CheckupMenuController {
+import java.io.IOException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class CheckupMenuController extends DatabaseConnection implements Initializable {
+
+    @FXML
+    private Label queueSelectLabel;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label ageLabel;
+    @FXML
+    private Label sexLabel;
+    @FXML
+    private Label phoneNumberLabel;
+    @FXML
+    private Label status1Label;
+    @FXML
+    private Label status2Label;
+    @FXML
+    private Label status3Label;
+    @FXML
+    private Label status4Label;
+    @FXML
+    private Rectangle status1Rectangle;
+    @FXML
+    private Rectangle status2Rectangle;
+    @FXML
+    private Rectangle status3Rectangle;
+    @FXML
+    private Rectangle status4Rectangle;
     @FXML
     private Button switchUserButton;
+    @FXML
+    private Button heightAndWeightButton;
+    @FXML
+    private Button snellensTestButton;
+    @FXML
+    private Button hearingTestButton;
+    @FXML
+    private Button historyButton;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private TextField queueNumberTextField;
+    @FXML
+    private Pane particularsPane;
+
+
+    private FXMLLoader fxmlLoader;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //fxmlLoader = new FXMLLoader();
+
+        // Add a listener to the text property of the queueNumberTextField
+        queueNumberTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                // Hide particularsPane when typing starts
+                if (newValue != null && !newValue.isEmpty()) {
+                    particularsPane.setVisible(false);
+                }
+            }
+        });
+
+        particularsPane.setVisible(false); // Initially hide the particularsPane
+    }
+
+    void loadFXML(String fxmlFile, ActionEvent e) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource(fxmlFile));
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
 
     @FXML
     public void switchUserButtonOnAction(ActionEvent e) {
+        loadFXML("login-page.fxml", e);
+    }
+
+    @FXML
+    public void heightAndWeightButtonOnAction(ActionEvent e) {
+        loadFXML("height-and-weight.fxml", e);
+    }
+
+    @FXML
+    public void snellensTestButtonOnAction(ActionEvent e) {
+        loadFXML("snellens-test.fxml", e);
+    }
+
+    @FXML
+    public void hearingTestButtonOnAction(ActionEvent e) {
+        loadFXML("hearing-test.fxml", e);
+    }
+
+    @FXML
+    public void historyButtonOnAction(ActionEvent e) {
+        loadFXML("history.fxml", e);
+    }
+
+    @FXML
+    public void searchButtonOnAction(ActionEvent e) {
+        if (queueNumberTextField.getText().isEmpty()) {
+            Labels.showMessageLabel(queueSelectLabel, "Input a queue number.", false);
+        } else {
+            updateParticularsPane(Integer.parseInt(queueNumberTextField.getText()));
+            particularsPane.setVisible(true);
+        }
+    }
+
+    public void updateParticularsPane(int queueNumber) {
+        String patientQuery = "SELECT * FROM patientQueueTable WHERE queueNumber = " + queueNumber;
+        String bmiRecordQuery = "SELECT * FROM heightAndWeightTable WHERE queueNumber = " + queueNumber;
+        // String snellensRecordQuery = "";
+        // String hearingRecordQuery = "";
+        // String historyRecordQuery = "";
+
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("login-page.fxml"));
-            Stage newUserStage = new Stage();
-            Scene scene = new Scene(fxmlLoader.load(), 520, 400);
-            //newUserStage.initStyle(StageStyle.UNDECORATED);
-            newUserStage.setTitle("Pharmacy");
-            newUserStage.setScene(scene);
-            Stage stage = (Stage) switchUserButton.getScene().getWindow();
-            stage.close();
-            newUserStage.show();
-        } catch (Exception exc) {
+            Statement statement = DatabaseConnection.connection.createStatement();
+
+            // Fetch patient details
+            ResultSet patientResultSet = statement.executeQuery(patientQuery);
+            if (patientResultSet.next()) {
+                String name = patientResultSet.getString("name");
+                int age = patientResultSet.getInt("age");
+                String sex = patientResultSet.getString("sex");
+                String phoneNumber = patientResultSet.getString("phoneNumber");
+
+                nameLabel.setText(name);
+                ageLabel.setText(String.valueOf(age));
+                sexLabel.setText(sex);
+                phoneNumberLabel.setText(phoneNumber);
+            } else {
+                nameLabel.setText("");
+                ageLabel.setText("");
+                sexLabel.setText("");
+                phoneNumberLabel.setText("");
+                Labels.showMessageLabel(queueSelectLabel, "Patient does not exist", false);
+                status1Rectangle.setStyle("-fx-fill: #707070;");
+                status1Label.setText(" Not found");
+                status2Rectangle.setStyle("-fx-fill: #707070;");
+                status2Label.setText(" Not found");
+                status3Rectangle.setStyle("-fx-fill: #707070;");
+                status3Label.setText(" Not found");
+                status4Rectangle.setStyle("-fx-fill: #707070;");
+                status4Label.setText(" Not found");
+                return;
+            }
+
+            // Check BMI record
+            ResultSet bmiResultSet = statement.executeQuery(bmiRecordQuery);
+            if (bmiResultSet.next()) {
+                status1Rectangle.setStyle("-fx-fill: #9dd895;");
+                status1Label.setText(" Complete");
+            } else {
+                status1Rectangle.setStyle("-fx-fill: #fa8072;");
+                status1Label.setText("Incomplete");
+            }
+
+            // ResultSet snellensResultSet = statement.executeQuery(snellensRecordQuery);
+            // Update status based on snellensResultSet
+
+            // ResultSet hearingResultSet = statement.executeQuery(hearingRecordQuery);
+            // Update status based on hearingResultSet
+
+            // ResultSet historyResultSet = statement.executeQuery(historyRecordQuery);
+            // Update status based on historyResultSet
+
+            // Close the statement
+            statement.close();
+        } catch (SQLException exc) {
             exc.printStackTrace();
-            exc.getCause();
+            Labels.showMessageLabel(queueSelectLabel, "Database error occurred", false);
         }
     }
 }
--- src/main/java/com/orb/battambang/login/LoginPageController.java
+++ src/main/java/com/orb/battambang/login/LoginPageController.java
@@ -1,11 +1,12 @@
 package com.orb.battambang.login;
-import com.orb.battambang.MainApp;
-import com.orb.battambang.util.Labels;
 
+import com.orb.battambang.MainApp;
 import com.orb.battambang.connection.DatabaseConnection;
+import com.orb.battambang.util.Labels;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,11 +14,12 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import javax.xml.transform.Result;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class LoginPageController extends DatabaseConnection{
+
+    double x, y;
 
     @FXML
     private Label loginMessageLabel;
@@ -68,23 +70,24 @@ public class LoginPageController extends DatabaseConnection{
                 if (resultSet.getInt(1) == 1) {
                     Stage stage = (Stage) loginMessageLabel.getScene().getWindow();
                     String role = getRoleByUsername(username);
-                    if (role.equals("Reception")) {
-                        openPatientRegistration();
-                        stage.close();
-                    } else if (role.equals("CheckUpStation")) {
-                        stage.close();
-                        openCheckupMenu();
-
-                    } else if (role.equals("Doctor")) {
-                        stage.close();
-                        openDoctorConsult();
-
-                    } else if (role.equals("Pharmacy")) {
-                        stage.close();
-                        openMedicineDispense();
-
-                    } else {
-                        stage.close();
+                    switch (role) {
+                        case "Reception" -> {
+                            openPatientRegistration();
+                            stage.close();
+                        }
+                        case "CheckUpStation" -> {
+                            stage.close();
+                            openCheckupMenu();
+                        }
+                        case "Doctor" -> {
+                            stage.close();
+                            openDoctorConsult();
+                        }
+                        case "Pharmacy" -> {
+                            stage.close();
+                            openMedicineDispense();
+                        }
+                        default -> stage.close();
                     }
 
                 } else {
@@ -124,7 +127,7 @@ public class LoginPageController extends DatabaseConnection{
             FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("patient-registration.fxml"));
             Stage newUserStage = new Stage();
             Scene scene = new Scene(fxmlLoader.load(), 1080 , 600);
-            newUserStage.setTitle("reception");
+            newUserStage.setTitle("Reception");
             //newUserStage.initStyle(StageStyle.UNDECORATED);
             newUserStage.setScene(scene);
             newUserStage.show();
@@ -135,26 +138,42 @@ public class LoginPageController extends DatabaseConnection{
     }
 
     public void openCheckupMenu() {
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("checkup-menu.fxml"));
-            Stage newUserStage = new Stage();
-            Scene scene = new Scene(fxmlLoader.load(), 1080 , 600);
-            newUserStage.setTitle("Reception");
-            //newUserStage.initStyle(StageStyle.UNDECORATED);
-            newUserStage.setScene(scene);
-            newUserStage.show();
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+
+            // Add the event handler to the root node or any specific node
+            root.setOnMousePressed(event -> {
+                x = event.getSceneX();
+                y = event.getSceneY();
+            });
+
+            root.setOnMouseDragged(event -> {
+                stage.setX(event.getScreenX());
+                stage.setY(event.getScreenY());
+            });
+
+            stage.setTitle("Check-up Stations");
+            // newUserStage.initStyle(StageStyle.UNDECORATED);
+
+            Scene scene = new Scene(root, 1080, 600);
+            stage.setScene(scene);
+            stage.show();
         } catch (Exception e) {
             e.printStackTrace();
             e.getCause();
         }
     }
 
+
     public void openDoctorConsult() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("doctor-consult.fxml"));
             Stage newUserStage = new Stage();
             Scene scene = new Scene(fxmlLoader.load(), 1080 , 600);
-            newUserStage.setTitle("Reception");
+            newUserStage.setTitle("Doctor Consult");
             //newUserStage.initStyle(StageStyle.UNDECORATED);
             newUserStage.setScene(scene);
             newUserStage.show();
@@ -169,7 +188,7 @@ public class LoginPageController extends DatabaseConnection{
             FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("medicine-dispense.fxml"));
             Stage newUserStage = new Stage();
             Scene scene = new Scene(fxmlLoader.load(), 1080 , 600);
-            newUserStage.setTitle("Reception");
+            newUserStage.setTitle("Pharmacy");
             //newUserStage.initStyle(StageStyle.UNDECORATED);
             newUserStage.setScene(scene);
             newUserStage.show();
